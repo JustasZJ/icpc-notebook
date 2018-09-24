f83935bec150f2082d8c5b491cd47ba095789090
JustasZJ <zjustas2000@gmail.com>
2018-09-24 17:48:08 +0300

    Modified LCA template

--- Lowest Common Ancestor.cpp
+++ Lowest Common Ancestor.cpp
@@ -13,47 +13,33 @@ typedef long double ld;
 typedef pair<int, int>pii;
 const int maxn=1e5+100;
 const int maxlog=20;
-int depth[maxn];
-int par[maxn][maxlog];
+int dep[maxn], anc[maxn][maxlog];
 vector<int>adj[maxn];
-int n;
-void dfs(int v, int p=-1)
+void dfs(int v)
 {
-    par[v][0]=p;
-    if(p!=-1)depth[v]=depth[p]+1;
     for(int i=1; i<maxlog; i++)
-        if(par[v][i-1]!=-1)
-            par[v][i]=par[par[v][i-1]][i-1];
+        anc[v][i]=anc[anc[v][i-1]][i-1];
     for(auto to:adj[v])
     {
-        if(to==p)continue;
-        dfs(to, v);
+        if(to==anc[v][0])continue;
+        anc[to][0]=v;
+        dep[to]=dep[v]+1;
+        dfs(to);
     }
 }
 int lca(int v, int u)
 {
-    if(depth[v]<depth[u])swap(v, u);
+    if(dep[v]<dep[u])swap(v, u);
     for(int i=maxlog-1; i>=0; i--)
-        if(par[v][i]!=-1&&depth[par[v][i]]>=depth[u])
-            v=par[v][i];
+        if(anc[v][i] && dep[anc[v][i]]>=dep[u])
+            v=anc[v][i];
     if(v==u)return v;
     for(int i=maxlog-1; i>=0; i--)
-        if(par[v][i]!=par[u][i])
-            v=par[v][i], u=par[u][i];
-    return par[v][0];
+        if(anc[v][i]!=anc[u][i])
+            v=anc[v][i], u=anc[u][i];
+    return anc[v][0];
 }
-int main()
+int getDist(int v, int u)
 {
-    memset(par, -1, sizeof par);
-    ios_base::sync_with_stdio(false), cin.tie(0);
-    cin>>n;
-    for(int i=0; i<n-1; i++)
-    {
-        int a, b;
-        cin>>a>>b;
-        adj[a].pb(b);
-        adj[b].pb(a);
-    }
-    dfs(1);
-    return 0;
+    return dep[v]+dep[u]-2*dep[lca(v, u)];
 }
