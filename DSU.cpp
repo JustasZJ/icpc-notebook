d497194175166496c1fbd2ba5b3ee17bd1acf1ae
JustasZJ <zjustas2000@gmail.com>
2018-09-24 22:08:23 +0300

    Added DSU template

---
+++ DSU.cpp
@@ -0,0 +1,20 @@
+template<int N>struct DSU
+{
+    int par[N+1], sz[N+1];
+    DSU()
+    {
+        for(int i=0; i<=N; i++)par[i]=i, sz[i]=1;
+    }
+    int root(int v)
+    {
+        return v==par[v]?v:par[v]=root(par[v]);
+    }
+    bool unite(int a, int b)
+    {
+        if((a=root(a))==(b=root(b)))return false;
+        if(sz[a]<sz[b])swap(a, b);
+        sz[a]+=sz[b];
+        par[b]=a;
+        return true;
+    }
+};
