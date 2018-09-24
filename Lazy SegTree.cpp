a1195bafb0a24af7ab16c332b8f608fe0f22dd39
JustasZJ <zjustas2000@gmail.com>
2018-09-24 17:54:07 +0300

    modified Lazy SegTree for [l, r] operations

--- Lazy SegTree.cpp
+++ Lazy SegTree.cpp
@@ -17,33 +17,33 @@ template<class T, int N>struct SegTree
         memset(seg, 0, sizeof seg);
         memset(lazy, 0, sizeof lazy);
     }
-    void build(T arr[], int id=1, int l=0, int r=N)
+    void build(T arr[], int id=1, int l=0, int r=N-1)
     {
-        if(r-l<2)
+        if(r==l)
         {
             seg[id]=arr[l];
             return;
         }
         int mid=(l+r)/2;
         build(arr, id*2, l, mid);
-        build(arr, id*2+1, mid, r);
+        build(arr, id*2+1, mid+1, r);
         seg[id]=seg[id*2]+seg[id*2+1];
     }
     void upd(int id, int l, int r, T val)
     {
-        seg[id]+=(T)(r-l)*val;
+        seg[id]+=(T)(r-l+1)*val;
         lazy[id]+=val;
     }
     void shift(int id, int l, int r)
     {
         int mid=(l+r)/2;
         upd(id*2, l, mid, lazy[id]);
-        upd(id*2+1, mid, r, lazy[id]);
+        upd(id*2+1, mid+1, r, lazy[id]);
         lazy[id]=0;
     }
-    void modify(int x, int y, T val, int id=1, int l=0, int r=N)
+    void modify(int x, int y, T val, int id=1, int l=0, int r=N-1)
     {
-        if(l>=y || r<=x)return;
+        if(l>y || r<x)return;
         if(l>=x && r<=y)
         {
             upd(id, l, r, val);
@@ -52,26 +52,15 @@ template<class T, int N>struct SegTree
         shift(id, l, r);
         int mid=(l+r)/2;
         modify(x, y, val, id*2, l, mid);
-        modify(x, y, val, id*2+1, mid, r);
+        modify(x, y, val, id*2+1, mid+1, r);
         seg[id]=seg[id*2]+seg[id*2+1];
     }
-    T query(int x, int y, int id=1, int l=0, int r=N)
+    T query(int x, int y, int id=1, int l=0, int r=N-1)
     {
-        if(l>=y || r<=x)return 0;
+        if(l>y || r<x)return 0;
         if(l>=x && r<=y)return seg[id];
         shift(id, l, r);
         int mid=(l+r)/2;
-        return query(x, y, id*2, l, mid)+query(x, y, id*2+1, mid, r);
+        return query(x, y, id*2, l, mid)+query(x, y, id*2+1, mid+1, r);
     }
 };
-int main()
-{
-    ios_base::sync_with_stdio(false), cin.tie(0);
-    SegTree<ll, 100000>tree;
-    tree.modify(1, 6, 2);
-    tree.modify(5, 7, 3);
-    cout<<tree.query(5, 7)<<"\n";
-    cout<<tree.query(1, 7)<<"\n";
-    cout<<tree.query(1, 4)<<"\n";
-    return 0;
-}
