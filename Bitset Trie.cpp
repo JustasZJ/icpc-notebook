6caec3f3297abf9870aa219a7284ab231ba473f4
JustasZJ <zjustas2000@gmail.com>
2018-09-26 22:21:45 +0300

    Added Bitset Trie template

---
+++ Bitset Trie.cpp
@@ -0,0 +1,32 @@
+template<int N>struct trie
+{
+    int t[N+1][2], nxt=1;
+
+    trie()
+    {
+        memset(t, 0, sizeof t);
+    }
+
+    void ins(string s)
+    {
+        int cur=0;
+        for(int i=0; i<sz(s); ++i)
+        {
+            int bit=s[i]-'0';
+            if(!t[cur][bit])t[cur][bit]=nxt++;
+            cur=t[cur][bit];
+        }
+    }
+
+    bool fnd(string s)
+    {
+        int cur=0;
+        for(int i=0; i<sz(s); ++i)
+        {
+            int bit=s[i]-'0';
+            if(!t[cur][bit])return false;
+            cur=t[cur][bit];
+        }
+        return true;
+    }
+};
