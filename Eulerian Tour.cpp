7918a6afb5cf0c98bd49d8f67debd95729846733
JustasZJ <zjustas2000@gmail.com>
2019-03-12 21:00:17 +0200

    Added Eulerian Tour algorithm

--- Eulerian Tour.cpp
+++ Eulerian Tour.cpp
@@ -1,18 +1,3 @@
-#include <bits/stdc++.h>
-#define pb push_back
-#define all(a) a.begin(), a.end()
-#define sz(a) (int)a.size()
-#define x first
-#define y second
-#define debug(...) cout << "[" << #__VA_ARGS__ << ": " << __VA_ARGS__ << "]\n"
-#define rd() abs((int)rng())
-using namespace std;
-typedef long long ll;
-typedef long double ld;
-typedef pair<int, int>pii;
-const int maxn = 1e5 + 100;
-const int mod = 1e9 + 7;
-mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
 int n, m, even;
 set<int>adj[maxn];
 vector<int>tour;
@@ -20,10 +5,10 @@ void dfs(int v)
 {
 	for(auto it = adj[v].begin(); sz(adj[v]) > 0;)
 	{
-	    int to = *it;
-	    it = adj[v].erase(it);
-	    adj[to].erase(v);
-	    dfs(to);
+		int to = *it;
+		it = adj[v].erase(it);
+		adj[to].erase(v);
+		dfs(to);
 	}
 	tour.pb(v);
 }
@@ -41,7 +26,7 @@ int main()
     for(int i = 1; i <= n; i++)
     {
     	if(sz(adj[i]) % 2 == 0)
-    		++even;
+			++even;
     }
     if(even != n && even != n - 2)
     {
@@ -51,14 +36,14 @@ int main()
     int start = 1;
     if(even == n - 2)
     {
-    	for(int i = 1; i <= n; i++)
-    	{
-    		if(sz(adj[i]) % 2 == 1)
-    		{
-    			start = i;
-    			break;
-    		}
-    	}
+		for(int i = 1; i <= n; i++)
+		{
+			if(sz(adj[i]) % 2 == 1)
+			{
+				start = i;
+				break;
+			}
+		}
     }
     dfs(start);
     for(int v : tour)
