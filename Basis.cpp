680bb9b6c198c42c962c1575e5c71537e83b63f0
unknown <zjustas2000@gmail.com>
2019-09-23 23:05:23 +0100

    Finding basis of vectors modulo 2

---
+++ Basis.cpp
@@ -0,0 +1,20 @@
+// Basis of vectors modulo 2 (for xor related problems)
+// https://codeforces.com/blog/entry/68953
+
+ll basis[62];
+void add(ll a)
+{
+	for(int bit = 61; bit >= 0; bit--)
+	{
+		if(a >> bit & 1)
+		{
+			if(basis[bit])
+				a ^= basis[bit];
+			else
+			{
+				basis[bit] = a;
+				return;
+			}
+		}
+	}
+}
